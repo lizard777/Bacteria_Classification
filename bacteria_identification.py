a439f1eac1ffdde4eaee3067b9df7179cfdc70f2
lizard777 <79130777+lizard777@users.noreply.github.com>
2023-01-30 09:47:14 -0800

    Update bacteria_identification.py

--- bacteria_identification.py
+++ bacteria_identification.py
@@ -1,4 +1,10 @@
-from pandas import read_csv
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#for machine learning practice
+"""""
 from pandas.plotting import scatter_matrix
 from matplotlib import pyplot
 from sklearn.model_selection import train_test_split
@@ -13,4 +19,40 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svs import SVC
+"""
+
+import seaborn as sb
+
+
+#express genes as heatmap to see patterns if any 
+def initial_explatory(x): 
+    print(x.head())
+
+    x = x.nunique().sort_values(ascending=False)
+    print(x)
+
+
+"""
+
+    plt.figure(figsize=(5,15))
+    
+    sb.heatmap(x,linewidths = 0.5, linecolor = 'black', square = True, cmap = 'RdBu')
+    plt.xlabel('bacterial species', size = 15)
+    plt.ylabel('gene location', size = 15)
+    plt.title('gene vs. bacteria species', size = 15)
+    plt.xticks(rotation = 25)
+    plt.yticks(rotation = 0)
+    plt.savefig('test.png')
+    plt.show()
+"""
+
+
+
+
+
+
+
+
+data_set = pd.read_csv('test_dataset.csv',sep = ",")
+initial_explatory(data_set)
 
