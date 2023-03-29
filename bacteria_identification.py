839d7fa115f34ba1e55d66a730726862807e29a5
lizard777 <79130777+lizard777@users.noreply.github.com>
2023-03-28 17:37:57 -0700

    lazy predict
    trying to understand lazy predict

--- bacteria_identification.py
+++ bacteria_identification.py
@@ -2,8 +2,46 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from lazypredict.Supervised import LazyClassifier
+
+#must split the data to test and train data 
+train = pd.read_csv('train_dataset.csv',sep = ',')
+print(train.head())
+
+test = pd.read_csv('test_dataset.csv',sep = ",")
+print(test.head())
+
+#initial_explatory(data_set)
+
+#find unique falues of the train data 
+
+find_uniques = train.nunique().sort_values(ascending = False)
+print(find_uniques)
+print(train.info())
+
+#dropping row id from the data set as we are only interested in the expressed sequences 
+
+train = train.drop(['row_id'],axis = 1)
+print(train.head())
+
+test = test.drop(['row_id'], axis = 1)
+print(test.head())
+
+#reducing the memory usage 
+#def reduce_mem_usage(props):
+ #   start_mem_usg = props.memory_usage()
+
 
 #for machine learning practice
+y = train.pop('target')
+x = train
+#test size is 20%
+#random_state
+#shuffle
+#statify
+x_train, x_test,y_train,y_test = train_test_split( x, y, test_size=0.2, random_state=42,shuffle=True, stratify=y)
+
 """""
 from pandas.plotting import scatter_matrix
 from matplotlib import pyplot
@@ -32,6 +70,9 @@ def initial_explatory(x):
     print(x)
 
 
+
+
+
 """
 
     plt.figure(figsize=(5,15))
@@ -47,12 +88,4 @@ def initial_explatory(x):
 """
 
 
-
-
-
-
-
-
-data_set = pd.read_csv('test_dataset.csv',sep = ",")
-initial_explatory(data_set)
 
